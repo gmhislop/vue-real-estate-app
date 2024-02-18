f034fb67034d52b46bb50c40d1f21532100e35d8
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2024-02-18 18:06:21 +0100

    feat - create size/price sort option

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,11 +8,19 @@
                 <h2>Houses</h2>
                 <button class="createButton" @click="createNew">+ CREATE NEW</button>
             </div>
-            <Card v-for="(house, index) in houses" :key="index" :house="house" />
+            <div class="toggle-container">
+                <button class="toggle-button-left" :class="{ 'active': sortBy === 'price' }" @click="setSortBy('price')">
+                    Price
+                </button>
+                <button class="toggle-button-right" :class="{ 'active': sortBy === 'size' }" @click="setSortBy('size')">
+                    Size
+                </button>
+            </div>
+            <Card v-for="(house, index) in sortedHouses" :key="index" :house="house" />
         </div>
     </div>
 </template>
-  
+
 <script>
 import EmptyState from '@/components/EmptyState.vue';
 import Card from '@/components/Card.vue';
@@ -26,6 +34,7 @@ export default {
     data() {
         return {
             shouldShowEmptyState: false,
+            sortBy: 'price',
             houses: [ // Dummy data for houses
                 { address: '1234 Main St', price: '500.000', street: '1011AA Amsterdam', bathroom: 2, bedroom: 3, area: 100, image: 'https://via.placeholder.com/150', construction_date: '2020', garage: 2 },
                 { address: '5678 Elm St', price: '600.000', street: '1011AA Amsterdam', bathroom: 3, bedroom: 4, area: 120, image: 'https://via.placeholder.com/150', construction_date: '2021', garage: 2 },
@@ -41,15 +50,23 @@ export default {
         },
         createNew() {
             // Method logic for creating new house
+        },
+        setSortBy(option) {
+            this.sortBy = option;
+        }
+    },
+    computed: {
+        sortedHouses() {
+            return this.houses.slice().sort((a, b) => {
+                return this.sortBy === 'price' ? a.price - b.price : a.size - b.size;
+            });
         }
     }
 };
 </script>
-  
+
 <style scoped>
 .home {
-    /* width: 100%; */
-    height: 100%;
     padding: 1rem 10vw;
 }
 
@@ -77,6 +94,34 @@ h2 {
     font-weight: 700;
 }
 
+.toggle-container {
+    display: flex;
+    background-color: #C3C3C3;
+    border-radius: 8px;
+}
+
+.toggle-container button {
+    border: none;
+    padding: 0.5rem 2rem;
+    cursor: pointer;
+    color: black;
+}
+
+.toggle-button-left {
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+}
+
+.toggle-button-right {
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
+
+.toggle-container button.active {
+    background-color: #EB5440;
+    color: white;
+}
+
 .createButton {
     background-color: #EB5440;
     color: white;
@@ -86,4 +131,3 @@ h2 {
     cursor: pointer;
 }
 </style>
-  
