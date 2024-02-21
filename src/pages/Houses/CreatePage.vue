31f837a3c5227d43e622fb924dca51f846dbdd98
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2024-02-21 14:11:54 +0100

    improvement - Create house page

--- src/pages/Houses/CreatePage.vue
+++ src/pages/Houses/CreatePage.vue
@@ -1,54 +1,80 @@
 <template>
-    <div class="add">
-        <div class="add-container">
-            <router-link class="back-link" to="/"><img src="@/assets/ic_back_grey@3x.png" class="back-icon"
-                    alt="back" />Back to
-                overview</router-link>
-            <HousingForm :listing=null />
-        </div>
-    </div>
-</template>
-
-<script>
-import HousingForm from '@/components/HousingForm.vue';
-
-export default {
-    name: 'Add',
-    components: {
-        HousingForm
+    <layout class="layout-wrapper-details">
+      <section class="create-house-details">
+        <heading page-title="Create new listing"/>
+        <house-form :handle-submit="handleSubmit" :values="newHouse" button-text="POST"/>
+      </section>
+    </layout>
+  </template>
+  
+  <script setup>
+  import { useHousesStore } from '@/stores/houses'
+  import Layout from '@/components/templates/Layout/Layout.vue';
+  import Heading from '@/components/atoms/typography/Heading.vue';
+  import { useRouter } from 'vue-router';
+  import HouseForm from '@/components/HouseForm/HouseForm.vue'
+  import { DETAIL_PAGE } from '@/router';
+  
+  const newHouse = {
+    location: {
+      streetName: '', 
+      houseNumber: '',
+      zip: '',
+      city: '', 
+    },
+    numberAddition: '',
+    image: '',
+    price: '',
+    size: '',
+    hasGarage: '',
+    rooms: {
+      bedrooms: '',
+      bathrooms: '',
+    },
+    constructionYear: '',
+    description: ''
+  }
+  
+  const router = useRouter();
+  const storeHouses = useHousesStore();
+  
+  async function handleSubmit(formData, houseImageFile) {
+    const savedHouse = await storeHouses.createHouse(formData, houseImageFile)
+    router.push({name: DETAIL_PAGE, params: {id: savedHouse.id}})
+  }
+  </script>
+  
+  <style scoped>
+  .layout-wrapper-details {
+    padding-bottom: 4rem;
+    background-image: url("@/assets/img_background@3x.png");
+    background-repeat: no-repeat;
+    background-size: 130%;
+    background-position: right -6.25rem bottom;
+  }
+  
+  .create-house-details {
+    max-width: 28.125rem;
+    margin: auto;
+  }
+  
+  @media screen and (min-width: 48rem) {
+    .create-house-details {
+      max-width: 31.25rem;
     }
-};
-</script>
-
-<style scoped>
-.add {
-    background-image: url('@/assets/img_background@3x.png');
-    background-size: cover;
-    width: 100%;
-    height: 100%;
-}
-
-.add-container {
-    max-width: 800px;
-    padding: 1rem 10vw;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-}
-
-.back-icon {
-    margin-right: 0.5rem;
-    height: 1rem;
-}
-
-.back-link {
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    font-weight: 700;
-    color: var(--color-text);
-    cursor: pointer;
-    width: fit-content;
-    margin: 1rem 2rem 0 0;
-}
-</style>
+  }
+  
+  @media screen and (min-width: 64rem) {
+    .create-house-details {
+      max-width: 23.625rem;
+      margin: 0;
+    }
+  
+    .layout-wrapper-details {
+      background-size: cover;
+      background-position: right top;
+    }
+  }
+  </style>
+  
+  
